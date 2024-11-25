33b884cc6c8e42c5cfefd67d6c279c80bb40c795
BrahiamAcosta <brahiam.acosta@udea.edu.co>
2024-11-25 16:26:16 -0500

    First commit

---
+++ src/main/java/com/udea/websocketsair/WebsocketsairApplication.java
@@ -0,0 +1,13 @@
+package com.udea.websocketsair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketsairApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketsairApplication.class, args);
+    }
+
+}
